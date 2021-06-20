5d027acf30697f3cccefcd0e8ea75cdcf8fd7ea6
prabhuashish <76871941+prabhuashish@users.noreply.github.com>
2021-06-20 12:30:04 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,6 +1,6 @@
 import math
 
-radius = 1.1
+radius = int(input("Enter the radius of the circle: "))
 area = math.pi * radius * radius
 print(f'The area of the circle is {area:.3f}')
 # round answer
